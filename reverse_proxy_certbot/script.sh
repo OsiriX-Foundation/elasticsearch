f05f2678205cef9c525727ce2f3e1df285543b11
Ansible user <ansible@medirad15.cbgz.vital-it.ch>
2020-12-14 10:33:10 +0100

    save config

--- reverse_proxy_certbot/script.sh
+++ reverse_proxy_certbot/script.sh
@@ -4,18 +4,26 @@ missing_env_var_secret=false
 
 
 #Verify environment variables
-if [[ -z $KHEOPS_ROOT_SCHEME ]]; then
-  echo "Missing KHEOPS_ROOT_SCHEME environment variable"
+if [[ -z $NGINX_ROOT_SCHEME ]]; then
+  echo "Missing NGINXS_ROOT_SCHEME environment variable"
   missing_env_var_secret=true
 else
-   echo -e "environment variable KHEOPS_ROOT_SCHEME \e[92mOK\e[0m"
+   echo -e "environment variable NGINX_ROOT_SCHEME \e[92mOK\e[0m"
 fi
-if [[ -z $KHEOPS_ROOT_HOST ]]; then
-  echo "Missing KHEOPS_ROOT_HOST environment variable"
+if [[ -z $NGINX_ROOT_HOST ]]; then
+  echo "Missing NGINX_ROOT_HOST environment variable"
   missing_env_var_secret=true
 else
-   echo -e "environment variable KHEOPS_ROOT_HOST \e[92mOK\e[0m"
+   echo -e "environment variable NGINX_ROOT_HOST \e[92mOK\e[0m"
 fi
+
+if [[ -z $NGINX_ROOT_URL ]]; then
+  echo "Missing NGINX_ROOT_URL environment variable"
+  missing_env_var_secret=true
+else
+   echo -e "environment variable NGINX_ROOT_URL \e[92mOK\e[0m"
+fi
+
 
 if [[ -z $LETS_ENCRYPT_EMAIL ]]; then
   echo "Missing LETS_ENCRYPT_EMAIL environment variable"
@@ -32,15 +40,11 @@ else
    echo -e "all nginx secrets and all env var \e[92mOK\e[0m"
 fi
 
-
-export roothost="$(awk -F/ '{sub("^[^@]+@","",$3); print $3}' <<<$KHEOPS_ROOT_URL)"
-
 #get env var
 chmod a+w /etc/nginx/conf.d/reverseproxy.conf
-sed -i "s|\${root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
+sed -i "s|\${root_url}|$NGINX_ROOT_SCHEME://$NGINX_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
 
-sed -i "s|\${server_name}|$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
+sed -i "s|\${server_name}|$NGINX_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
 
 echo "Ending setup NGINX secrets and env var"
 
-/etc/nginx/conf.d/nginx_certbot_script.sh
