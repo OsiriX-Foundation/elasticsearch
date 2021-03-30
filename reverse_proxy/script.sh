90d1951fa176e1fdf5571075458e0a64c544aaaa
gerome12 <gerome.pasquier@gmail.com>
2021-03-30 17:58:27 +0200

    Update script.sh

--- reverse_proxy/script.sh
+++ reverse_proxy/script.sh
@@ -2,34 +2,18 @@
 
 missing_env_var_secret=false
 
-
-#Verify secrets
-if ! [ -f ${SECRET_FILE_PATH}/privkey1.pem ]; then
-    echo "Missing privkey1.pem secret"
-    missing_env_var_secret=true
-else
-   echo -e "secret privkey1.pem \e[92mOK\e[0m"
-fi
-if ! [ -f ${SECRET_FILE_PATH}/fullchain1.pem ]; then
-    echo "Missing fullchain1.pem secret"
-    missing_env_var_secret=true
-else
-   echo -e "secret fullchain1.pem \e[92mOK\e[0m"
-fi
-
-
 #Verify environment variables
-if [[ -z $KHEOPS_ROOT_SCHEME ]]; then
-  echo "Missing KHEOPS_ROOT_SCHEME environment variable"
+if [[ -z $NGINX_ROOT_SCHEME ]]; then
+  echo "Missing NGINX_ROOT_SCHEME environment variable"
   missing_env_var_secret=true
 else
-   echo -e "environment variable KHEOPS_ROOT_SCHEME \e[92mOK\e[0m"
+   echo -e "environment variable NGINX_ROOT_SCHEME \e[92mOK\e[0m"
 fi
-if [[ -z $KHEOPS_ROOT_HOST ]]; then
-  echo "Missing KHEOPS_ROOT_HOST environment variable"
+if [[ -z $NGINX_ROOT_HOST ]]; then
+  echo "Missing NGINX_ROOT_HOST environment variable"
   missing_env_var_secret=true
 else
-   echo -e "environment variable KHEOPS_ROOT_HOST \e[92mOK\e[0m"
+   echo -e "environment variable NGINX_ROOT_HOST \e[92mOK\e[0m"
 fi
 
 
@@ -42,9 +26,9 @@ fi
 
 #get env var
 chmod a+w /etc/nginx/conf.d/reverseproxy.conf
-sed -i "s|\${root_url}|$KHEOPS_ROOT_SCHEME://$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
+sed -i "s|\${root_url}|$NGINX_ROOT_SCHEME://$NGINX_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
 
-sed -i "s|\${server_name}|$KHEOPS_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
+sed -i "s|\${server_name}|$NGINX_ROOT_HOST|" /etc/nginx/conf.d/reverseproxy.conf
 
 echo "Ending setup NGINX secrets and env var"
 
